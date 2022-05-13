b2e5d4deb37967a4832898a3c88f7988ce172d3e
Vlad <sidav94@gmail.com>
2022-05-13 17:23:59 +0300

    Refactors directories.

--- resources.go
+++ resources.go
@@ -10,10 +10,10 @@ var (
 func loadResources() {
 	// init textures (temp.)
 	wallTexturesAtlas = make(map[string]*raycaster.Texture, 0)
-	wallTexturesAtlas["WALL"] = raycaster.InitTextureFromImageFile("textures/wall.png")
-	wallTexturesAtlas["DOOR"] = raycaster.InitTextureFromImageFile("textures/door.png")
+	wallTexturesAtlas["WALL"] = raycaster.InitTextureFromImageFile("resources/textures/wall.png")
+	wallTexturesAtlas["DOOR"] = raycaster.InitTextureFromImageFile("resources/textures/door.png")
 
 	// init sprites
 	spritesAtlas = make(map[string]*raycaster.SpriteStruct, 0)
-	spritesAtlas["proj"] = raycaster.InitSpriteFromImageFile("sprites/projectile.png")
+	spritesAtlas["proj"] = raycaster.InitSpriteFromImageFile("resources/sprites/projectile.png")
 }

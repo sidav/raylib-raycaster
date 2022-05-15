73b1551e4dc7651ee711fcd974faa4decf0cae73
sidav <sidav94@gmail.com>
2022-05-15 05:05:01 +0300

    Adds sprite sorting.

--- raycaster/r_things.go
+++ raycaster/r_things.go
@@ -7,6 +7,28 @@ import (
 func (r *Renderer) renderThings() {
 	camx, camy := r.cam.getCoordsWithOffset()
 	things := r.scene.GetListOfThings()
+	
+	// sort by distance from camera (descending)
+	// WARNING: breaks things list order!
+	for node1 := things.Front(); node1 != nil && node1.Next() != nil; node1 = node1.Next() {
+
+		t1 := node1.Value.(Thing)
+		t1x, t1y := t1.GetCoords()
+		dist1 := (camx - t1x) * (camx - t1x) + (camy - t1y) * (camy - t1y)
+
+		for node2 := node1.Next(); node2 != nil; node2 = node2.Next() {
+
+			t2 := node2.Value.(Thing)
+			t2x, t2y := t2.GetCoords()
+			dist2 := (camx - t2x) * (camx - t2x) + (camy - t2y) * (camy - t2y)
+
+			if dist2 > dist1 {
+				// swap 2 and 1
+				things.MoveAfter(node1, node2)
+			}
+		}
+	}
+	
 	for node := things.Front(); node != nil; node = node.Next() {
 		// unneeded? 
 		//if node.Value == nil {
@@ -38,6 +60,7 @@ func (r *Renderer) renderThings() {
 			if screenXCoord < 0 || screenXCoord > r.RenderWidth-1 || r.rayDistancesBuffer[screenXCoord] < transformY {
 				continue
 			}
+			// r.rayDistancesBuffer[screenXCoord] = transformY
 			spriteX := x * currSprite.w / osw
 
 			for y := 0; y < osh; y++ {
